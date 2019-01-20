1997c9264887cff298699db45770f16d9668b64b
= <=>
2019-01-20 13:48:48 +0800

    code intended and added comments

--- src/toy-robot.js
+++ src/toy-robot.js
@@ -1,74 +1,77 @@
 "use strict";
-let resultFace    = require("./face-map");
+let resultFace = require("./face-map");
 /**
  * ToyRobot class 
  */
-class ToyRobot{
+class ToyRobot {
 
-	constructor(){
+	// set default.
+	constructor() {
 		this.placed = false;
-		this.x     = null;
-		this.y     = null;
-		this.face  = null; 
+		this.x = null;
+		this.y = null;
+		this.face = null;
 	}
 
-	loadData(data){
+	// process input data.
+	loadData(data) {
 		var inpArray = data.toString().split("\n");
-		for(var i in inpArray) {
+		for (var i in inpArray) {
 			this.handleCmd(inpArray[i].toUpperCase());
 		}
 	}
 
-	checkPlaceCmd(cmd){
+	// check valid place cmd.
+	checkPlaceCmd(cmd) {
 		var inputCmd = cmd.split(" ")[0];
-		if ( inputCmd === "PLACE" ) {
+		if (inputCmd === "PLACE") {
 			return true;
 		}
 		return false;
 	}
 
-	placeRobot(cmd){
-		var arr = cmd.split(",").map(function(d) {
+	// place robot if valid cmd.
+	placeRobot(cmd) {
+		var arr = cmd.split(",").map(function (d) {
 			return d.replace("PLACE ", "");
 		});
 
 		if (!resultFace.hasOwnProperty(arr[2].toLowerCase())) return false;
 
-		if (parseInt(arr[0]) >= 0 && 
-        parseInt(arr[0]) <= 4 && 
-        parseInt(arr[1]) >= 0 && 
-        parseInt(arr[1]) <= 4) {
+		if (parseInt(arr[0]) >= 0 &&
+			parseInt(arr[0]) <= 4 &&
+			parseInt(arr[1]) >= 0 &&
+			parseInt(arr[1]) <= 4) {
 			this.x = parseInt(arr[0]);
 			this.y = parseInt(arr[1]);
 			this.placed = true;
 			this.face = arr[2];
-			//console.log(this);     
 			return this;
 		}
-    
+
 	}
 
-	handleCmd(cmd){
+	// check cmd line by line.
+	handleCmd(cmd) {
 		var inputCmd = cmd.trim().toUpperCase();
-		switch(true){
+		switch (true) {
 		case (this.checkPlaceCmd(inputCmd)):
 			this.placeRobot(inputCmd);
 			break;
-		case ((inputCmd == "LEFT") || (inputCmd == "RIGHT")) :
-			//console.log('turn:' + inputCmd);
+		case ((inputCmd == "LEFT") || (inputCmd == "RIGHT")):
 			this.turnDirection(inputCmd);
 			break;
-		case (inputCmd === "MOVE") :
+		case (inputCmd === "MOVE"):
 			this.moveAhead();
 			break;
-		case (inputCmd === "REPORT") : 
+		case (inputCmd === "REPORT"):
 			this.reportPosition();
 			break;
 		}
 	}
 
+	// move towards current facing.
 	moveAhead() {
-		//console.log('move: :' + this.placed);
 		if (this.placed) {
 			switch (this.face.toLowerCase()) {
 			case "north":
@@ -90,35 +93,34 @@ class ToyRobot{
 			}
 		}
 	}
-  
+
+	// report current position.
 	reportPosition() {
 		if (!this.placed) {
 			process.stdout.write("Robot is not placed yet. \n");
 			return;
 		}
-        
+
 		process.stdout.write(
 			"################################################" + "\n" +
-            "Report: " + this.x + "," + this.y + "," + this.face.toUpperCase() + "\n" +
-            "################################################"); 
+			"Report: " + this.x + "," + this.y + "," + this.face.toUpperCase() + "\n" +
+			"################################################");
 
 	}
 
-
+	// turn 90 deg if valid
 	turnDirection(cmd) {
-		// Ignore command if robot is not yet placed
 		if (!this.placed) {
 			return;
 		}
 
 		let currFace = this.face.toLowerCase();
-		let currCmd  = cmd.toLowerCase();
+		let currCmd = cmd.toLowerCase();
 		let dirKey = resultFace.hasOwnProperty(currFace);
-		//console.log('dir key: '+ dirKey);
-		if(dirKey){
+		if (dirKey) {
 			this.face = resultFace[currFace][currCmd];
 		}
-    
+
 		return this;
 	}
 }
